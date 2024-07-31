3d39ffb04908fda3bb72cc4c0976048c028fb577
The _IsaacAgboola <isaacadeyemi9@gmail.com>
2024-07-31 06:56:03 +0100

    css reset

--- src/Components/Nav.tsx
+++ src/Components/Nav.tsx
@@ -1,47 +1,49 @@
 import Logo from "./Logo";
 import { Link } from "react-router-dom";
 import { FaFacebookF, FaTwitter, FaInstagram } from "react-icons/fa";
-
+import styles from "../styles/Nav.module.css";
 export default function Nav() {
   return (
-    <nav>
-      <Logo ftSize={"24px"} />
+    <div className={styles["nav-container"]}>
+      <nav className={styles.nav}>
+        <Logo ftSize={"24px"} />
 
-      <ul>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/">Recipes</Link>
-        </li>
-        <li>
-          <Link to="/">Blog</Link>
-        </li>
-        <li>
-          <Link to="/">Contact</Link>
-        </li>
-        <li>
-          <Link to="/">About Us</Link>
-        </li>
-      </ul>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/">Recipes</Link>
+          </li>
+          <li>
+            <Link to="/">Blog</Link>
+          </li>
+          <li>
+            <Link to="/">Contact</Link>
+          </li>
+          <li>
+            <Link to="/">About Us</Link>
+          </li>
+        </ul>
 
-      <ul>
-        <li>
-          <Link to="/">
-            <FaFacebookF size={22} />
-          </Link>
-        </li>
-        <li>
-          <Link to="/">
-            <FaTwitter size={22} />
-          </Link>
-        </li>
-        <li>
-          <Link to="/">
-            <FaInstagram size={22} />
-          </Link>
-        </li>
-      </ul>
-    </nav>
+        <ul>
+          <li>
+            <Link to="/">
+              <FaFacebookF size={22} />
+            </Link>
+          </li>
+          <li>
+            <Link to="/">
+              <FaTwitter size={22} />
+            </Link>
+          </li>
+          <li>
+            <Link to="/">
+              <FaInstagram size={22} />
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </div>
   );
 }
